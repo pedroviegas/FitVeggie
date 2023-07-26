b311db80944438e2c60656746c6b2d10e17a63ca
Pedro Viegas <viegas.pedro@live.com>
2023-07-26 22:43:50 +0100

    Update home.view.php

--- backend/views/home.view.php
+++ backend/views/home.view.php
@@ -1,10 +1,10 @@
 <?php require'../controller/sidebar.php'; ?>  
 
-<!--Container da Página-->
+<!--Page Container-->
 <section class="page-container">
     <div class="page-content-wrapper">
 
-        <!--Conteúdo Principal-->
+        <!--Main Content-->
         <div class="content sm-gutter">
             <div class="container-fluid padding-25 sm-padding-10">
                 <div class="row">
@@ -96,29 +96,103 @@
                                     </li>
                                 </ul>
                             </div>
+                            </div>
+                            <div class="table-responsive">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Título</th>
+                                        <th scope="col">Visualizações</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach($exercises_recent as $exercise_item): ?>
+                                    <tr>
+                                        <th scope="row"><?php echo $exercise_item['exercise_id']; ?></th>
+                                        <td><a href="../controller/edit_exercise.php?id=<?php echo $exercise_item['exercise_id']; ?>"><?php echo $exercise_item['exercise_title']; ?></a></td>
+                                        <td><?php echo $exercise_item['exercise_views']; ?></td>
+                                    </tr>
+                                    <?php endforeach; ?>
+                                </tbody>
+                            </table>
+                            </div>
                         </div>
-                        <?php echo $exercises_latest; ?>
                     </div>
 
                     <div class="col-12 col-md-6 col-lg-6">
                         <div class="block table-block mb-4">
                             <div class="block-heading d-flex align-items-center" style="border:0; padding-bottom: 0;">
-                                <h5 class="text-truncate">Publicações Recentes</h5>
+                                <h5 class="text-truncate">Treinos</h5>
                                 <div class="graph-pills graph-home">
-                                    <ul class="nav nav-pills" id="pills-tab" role="tablist">
-                                        <li class="nav-item">
-                                            <a class="nav-link active active-2" href="../controller/posts.php">Ver Todos <i class="fa fa-angle-right" style="margin-left: 5px"></i></a>
-                                        </li>
-                                    </ul>
-                                </div>
+                                <ul class="nav nav-pills" id="pills-tab" role="tablist">
+                                    <li class="nav-item">
+                                        <a class="nav-link active active-2" href="../controller/workouts.php">Ver Todos <i class="fa fa-angle-right" style="margin-left: 5px"></i></a>
+                                    </li>
+                                </ul>
                             </div>
-                        <?php echo $posts_latest; ?>
+                            </div>
+                            <div class="table-responsive">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Título</th>
+                                        <th scope="col">Visualizações</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach($workouts_recent as $workout_item): ?>
+                                    <tr>
+                                        <th scope="row"><?php echo $workout_item['workout_id']; ?></th>
+                                        <td><a href="../controller/edit_workout.php?id=<?php echo $workout_item['workout_id']; ?>"><?php echo $workout_item['workout_title']; ?></a></td>
+                                        <td><?php echo $workout_item['workout_views']; ?></td>
+                                    </tr>
+                                    <?php endforeach; ?>
+                                </tbody>
+                            </table>
+                            </div>
+                        </div>
                     </div>
+
+                    <div class="col-12">
+                        <div class="section-title">
+                            <h4>Atividade Recente</h4>
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <div class="block activity-block mb-4">
+                            <div class="table-responsive">
+
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Nome</th>
+                                        <th scope="col">Email</th>
+                                        <th scope="col">Data da Inscrição</th>
+                                        <th scope="col">Assinatura</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach($users_recent as $user_item): ?>
+                                    <tr>
+                                        <th scope="row"><?php echo $user_item['user_id']; ?></th>
+                                        <td><a href="../controller/edit_user.php?id=<?php echo $user_item['user_id']; ?>"><?php echo $user_item['user_name']; ?></a></td>
+                                        <td><?php echo $user_item['user_email']; ?></td>
+                                        <td><?php echo date('d/m/Y', $user_item['user_date']); ?></td>
+                                        <td><?php echo ($user_item['user_subscriber'] == 1 ? '<span class="badge badge-pill badge-success">Sim</span>' : '<span class="badge badge-pill badge-danger">Não</span>'); ?></td>
+                                    </tr>
+                                    <?php endforeach; ?>
+                                </tbody>
+                            </table>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
     </div>
 </section>
-
-<?php require'../controller/footer.php'; ?>
-
