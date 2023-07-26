d02bf1460255bcdc79406cd1d17038216f499fce
Pedro Viegas <viegas.pedro@live.com>
2023-07-26 20:07:49 +0100

    menu

--- backend/views/sidebar.view.php
+++ backend/views/sidebar.view.php
@@ -4,7 +4,7 @@
     </div>
 
     <div class="welcome">
-        Welcome, <b><?php echo $user['manager_name']; ?></b> <a href="../controller/logout.php" class="sidebar-user"><i class="dripicons-exit"></i></a>
+        Bem vindo, <b><?php echo $user['manager_name']; ?></b> <a href="../controller/logout.php" class="sidebar-user"><i class="dripicons-exit"></i></a>
     </div>
 
     <!--Navigation Menu Links-->
@@ -15,7 +15,7 @@
             <li>
                 <a href="javascript:void(0);" class="have-submenu <?php showMenu('exercises.php');?>">
                     <span class="icon-thumbnail"><i class="dripicons-stopwatch"></i></span>
-                    <span class="title">Exercises</span>
+                    <span class="title">Exercícios</span>
                 </a>
                 <!--Submenu-->
                 <ul class="sub-menu" style="display: <?php activePage('exercises.php');?>">
@@ -23,14 +23,14 @@
                     <li>
                         <a href="../controller/exercises.php">
                             <span class="icon-thumbnail"><i class="dripicons-dot"></i></span>
-                            <span class="title">Show All</span>
+                            <span class="title">Mostrar Todos</span>
                         </a>
                     </li>
 
                     <li>
                         <a href="../controller/new_exercise.php">
                             <span class="icon-thumbnail"><i class="dripicons-dot"></i></span>
-                            <span class="title">New Exercise</span>
+                            <span class="title">Novo Exercício</span>
                         </a>
                     </li>
 
@@ -40,7 +40,7 @@
             <li>
                 <a href="javascript:void(0);" class="have-submenu <?php showMenu('workouts.php');?>">
                     <span class="icon-thumbnail"><i class="dripicons-to-do"></i></span>
-                    <span class="title">Workouts</span>
+                    <span class="title">Treinos</span>
                 </a>
                 <!--Submenu-->
                 <ul class="sub-menu" style="display: <?php activePage('workouts.php');?>">
@@ -48,14 +48,14 @@
                     <li>
                         <a href="../controller/workouts.php">
                             <span class="icon-thumbnail"><i class="dripicons-dot"></i></span>
-                            <span class="title">Show All</span>
+                            <span class="title">Mostrar Todos</span>
                         </a>
                     </li>
 
                     <li>
                         <a href="../controller/new_workout.php">
                             <span class="icon-thumbnail"><i class="dripicons-dot"></i></span>
-                            <span class="title">New Workout</span>
+                            <span class="title">Novo Treino</span>
                         </a>
                     </li>
 
@@ -66,7 +66,7 @@
             <li>
                 <a href="javascript:void(0);" class="have-submenu <?php showMenu('bodyparts.php');?>">
                     <span class="icon-thumbnail"><i class="dripicons-user"></i></span>
-                    <span class="title">Bodyparts</span>
+                    <span class="title">Partes do corpo</span>
                 </a>
                 <!--Submenu-->
                 <ul class="sub-menu" style="display: <?php activePage('bodyparts.php');?>">
@@ -74,14 +74,14 @@
                     <li>
                         <a href="../controller/bodyparts.php">
                             <span class="icon-thumbnail"><i class="dripicons-dot"></i></span>
-                            <span class="title">Show All</span>
+                            <span class="title">Mostrar Todos</span>
                         </a>
                     </li>
 
                     <li>
                         <a href="../controller/new_bodypart.php">
                             <span class="icon-thumbnail"><i class="dripicons-dot"></i></span>
-                            <span class="title">New Bodypart</span>
+                            <span class="title">Novas Partes do corpo</span>
                         </a>
                     </li>
 
@@ -92,7 +92,7 @@
             <li>
                 <a href="javascript:void(0);" class="have-submenu <?php showMenu('equipments.php');?>">
                     <span class="icon-thumbnail"><i class="dripicons-lifting"></i></span>
-                    <span class="title">Equipments</span>
+                    <span class="title">Equipamentos</span>
                 </a>
                 <!--Submenu-->
                 <ul class="sub-menu" style="display: <?php activePage('equipments.php');?>">
@@ -100,14 +100,14 @@
                     <li>
                         <a href="../controller/equipments.php">
                             <span class="icon-thumbnail"><i class="dripicons-dot"></i></span>
-                            <span class="title">Show All</span>
+                            <span class="title">Mostrar Todos</span>
                         </a>
                     </li>
 
                     <li>
                         <a href="../controller/new_equipment.php">
                             <span class="icon-thumbnail"><i class="dripicons-dot"></i></span>
-                            <span class="title">New Equipment</span>
+                            <span class="title">Novo Equipamento</span>
                         </a>
                     </li>
 
@@ -118,7 +118,7 @@
             <li>
                 <a href="javascript:void(0);" class="have-submenu <?php showMenu('levels.php');?>">
                     <span class="icon-thumbnail"><i class="dripicons-pulse"></i></span>
-                    <span class="title">Levels</span>
+                    <span class="title">Níveis</span>
                 </a>
                 <!--Submenu-->
                 <ul class="sub-menu" style="display: <?php activePage('levels.php');?>">
@@ -126,14 +126,14 @@
                     <li>
                         <a href="../controller/levels.php">
                             <span class="icon-thumbnail"><i class="dripicons-dot"></i></span>
-                            <span class="title">Show All</span>
+                            <span class="title">Mostrar Todos</span>
                         </a>
                     </li>
 
                     <li>
                         <a href="../controller/new_level.php">
                             <span class="icon-thumbnail"><i class="dripicons-dot"></i></span>
-                            <span class="title">New Level</span>
+                            <span class="title">Novo Nível</span>
                         </a>
                     </li>
 
@@ -144,7 +144,7 @@
             <li>
                 <a href="javascript:void(0);" class="have-submenu <?php showMenu('goals.php');?>">
                     <span class="icon-thumbnail"><i class="dripicons-trophy"></i></span>
-                    <span class="title">Goals</span>
+                    <span class="title">Metas</span>
                 </a>
                 <!--Submenu-->
                 <ul class="sub-menu" style="display: <?php activePage('goals.php');?>">
@@ -152,14 +152,14 @@
                     <li>
                         <a href="../controller/goals.php">
                             <span class="icon-thumbnail"><i class="dripicons-dot"></i></span>
-                            <span class="title">Show All</span>
+                            <span class="title">Mostrar Todos</span>
                         </a>
                     </li>
 
                     <li>
                         <a href="../controller/new_goal.php">
                             <span class="icon-thumbnail"><i class="dripicons-dot"></i></span>
-                            <span class="title">New Goal</span>
+                            <span class="title">Nova Meta</span>
                         </a>
                     </li>
 
@@ -169,7 +169,7 @@
             <li>
                 <a href="javascript:void(0);" class="have-submenu <?php showMenu('recipes.php');?>">
                     <span class="icon-thumbnail"><i class="dripicons-cutlery"></i></span>
-                    <span class="title">Recipes</span>
+                    <span class="title">Receitas</span>
                 </a>
                 <!--Submenu-->
                 <ul class="sub-menu" style="display: <?php activePage('recipes.php');?>">
@@ -177,14 +177,14 @@
                     <li>
                         <a href="../controller/recipes.php">
                             <span class="icon-thumbnail"><i class="dripicons-dot"></i></span>
-                            <span class="title">Show All</span>
+                            <span class="title">Mostrar Todos</span>
                         </a>
                     </li>
 
                     <li>
                         <a href="../controller/new_recipe.php">
                             <span class="icon-thumbnail"><i class="dripicons-dot"></i></span>
-                            <span class="title">New Recipe</span>
+                            <span class="title">Nova Receita</span>
                         </a>
                     </li>
 
@@ -195,7 +195,7 @@
             <li>
                 <a href="javascript:void(0);" class="have-submenu <?php showMenu('categories.php');?>">
                     <span class="icon-thumbnail"><i class="dripicons-folder"></i></span>
-                    <span class="title">Categories</span>
+                    <span class="title">Categorias</span>
                 </a>
                 <!--Submenu-->
                 <ul class="sub-menu" style="display: <?php activePage('categories.php');?>">
@@ -203,14 +203,14 @@
                     <li>
                         <a href="../controller/categories.php">
                             <span class="icon-thumbnail"><i class="dripicons-dot"></i></span>
-                            <span class="title">Show All</span>
+                            <span class="title">Mostrar Todos</span>
                         </a>
                     </li>
 
                     <li>
                         <a href="../controller/new_category.php">
                             <span class="icon-thumbnail"><i class="dripicons-dot"></i></span>
-                            <span class="title">New Category</span>
+                            <span class="title">Nova Categoria</span>
                         </a>
                     </li>
 
@@ -220,7 +220,7 @@
             <li>
                 <a href="javascript:void(0);" class="have-submenu <?php showMenu('posts.php');?>">
                     <span class="icon-thumbnail"><i class="dripicons-article"></i></span>
-                    <span class="title">Posts</span>
+                    <span class="title">Postagens</span>
                 </a>
                 <!--Submenu-->
                 <ul class="sub-menu" style="display: <?php activePage('posts.php');?>">
@@ -228,14 +228,14 @@
                     <li>
                         <a href="../controller/posts.php">
                             <span class="icon-thumbnail"><i class="dripicons-dot"></i></span>
-                            <span class="title">Show All</span>
+                            <span class="title">Mostrar Todos</span>
                         </a>
                     </li>
 
                     <li>
                         <a href="../controller/new_post.php">
                             <span class="icon-thumbnail"><i class="dripicons-dot"></i></span>
-                            <span class="title">New Post</span>
+                            <span class="title">Nova Postagem</span>
                         </a>
                     </li>
 
@@ -253,14 +253,14 @@
                     <li>
                         <a href="../controller/tags.php">
                             <span class="icon-thumbnail"><i class="dripicons-dot"></i></span>
-                            <span class="title">Show All</span>
+                            <span class="title">Mostrar Todos</span>
                         </a>
                     </li>
 
                     <li>
                         <a href="../controller/new_tag.php">
                             <span class="icon-thumbnail"><i class="dripicons-dot"></i></span>
-                            <span class="title">New Tag</span>
+                            <span class="title">Nova Tag</span>
                         </a>
                     </li>
 
@@ -270,7 +270,7 @@
             <li>
                 <a href="javascript:void(0);" class="have-submenu <?php showMenu('products.php');?>">
                     <span class="icon-thumbnail"><i class="dripicons-cart"></i></span>
-                    <span class="title">Affiliate Products</span>
+                    <span class="title">Produtos afiliados</span>
                 </a>
                 <!--Submenu-->
                 <ul class="sub-menu" style="display: <?php activePage('products.php');?>">
@@ -278,14 +278,14 @@
                     <li>
                         <a href="../controller/products.php">
                             <span class="icon-thumbnail"><i class="dripicons-dot"></i></span>
-                            <span class="title">Show All</span>
+                            <span class="title">Mostrar Todos</span>
                         </a>
                     </li>
 
                     <li>
                         <a href="../controller/new_product.php">
                             <span class="icon-thumbnail"><i class="dripicons-dot"></i></span>
-                            <span class="title">New Product</span>
+                            <span class="title">Novo Produtos afiliados</span>
                         </a>
                     </li>
 
@@ -295,7 +295,7 @@
             <li>
                 <a href="javascript:void(0);" class="have-submenu <?php showMenu('types.php');?>">
                     <span class="icon-thumbnail"><i class="dripicons-bookmarks"></i></span>
-                    <span class="title">Product Types</span>
+                    <span class="title">Tipos de produtos</span>
                 </a>
                 <!--Submenu-->
                 <ul class="sub-menu" style="display: <?php activePage('types.php');?>">
@@ -303,14 +303,14 @@
                     <li>
                         <a href="../controller/types.php">
                             <span class="icon-thumbnail"><i class="dripicons-dot"></i></span>
-                            <span class="title">Show All</span>
+                            <span class="title">Mostrar Todos</span>
                         </a>
                     </li>
 
                     <li>
                         <a href="../controller/new_type.php">
                             <span class="icon-thumbnail"><i class="dripicons-dot"></i></span>
-                            <span class="title">New Type</span>
+                            <span class="title">Novos Tipos de produtos</span>
                         </a>
                     </li>
 
@@ -320,7 +320,7 @@
             <li>
                 <a href="javascript:void(0);" class="have-submenu <?php showMenu('managers.php');?>">
                     <span class="icon-thumbnail"><i class="dripicons-user"></i></span>
-                    <span class="title">Managers</span>
+                    <span class="title">Gerentes</span>
                 </a>
                 <!--Submenu-->
                 <ul class="sub-menu" style="display: <?php activePage('managers.php');?>">
@@ -328,14 +328,14 @@
                     <li>
                         <a href="../controller/managers.php">
                             <span class="icon-thumbnail"><i class="dripicons-dot"></i></span>
-                            <span class="title">Show All</span>
+                            <span class="title">Mostrar Todos</span>
                         </a>
                     </li>
 
                     <li>
                         <a href="../controller/new_manager.php">
                             <span class="icon-thumbnail"><i class="dripicons-dot"></i></span>
-                            <span class="title">New Manager</span>
+                            <span class="title">Novo Gerente</span>
                         </a>
                     </li>
 
@@ -345,7 +345,7 @@
             <li>
                 <a href="../controller/users.php">
                     <span class="icon-thumbnail"><i class="dripicons-user-group"></i></span>
-                    <span class="title">Users</span>
+                    <span class="title">Utilizadores</span>
                 </a>
             </li>
 
@@ -371,7 +371,7 @@
                         <img src="../assets/images/wbdashboard-dark.png" class="logo-side-dark">
                     </div>
                     <div class="col-lg-8 d-none d-lg-block">
-                        <p class="sidebar-relative">Welcome, <b><?php echo $user['manager_name']; ?></b> <a href="../controller/logout.php" class="sidebar-logout"><i class="dripicons-exit"></i></a></p>
+                        <p class="sidebar-relative">Bem vindo, <b><?php echo $user['manager_name']; ?></b> <a href="../controller/logout.php" class="sidebar-logout"><i class="dripicons-exit"></i></a></p>
                     </div>
                     
                 </div>
